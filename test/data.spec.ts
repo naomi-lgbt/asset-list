a2cb2555425b44cbf139b129a7e22fb7ae8f1369
Naomi Carrigan <nhcarrigan@gmail.com>
2023-08-23 00:22:22 -0700

    feat: add melody to asset list (#43)

--- test/data.spec.ts
+++ test/data.spec.ts
@@ -19,7 +19,7 @@ const adventureList = ["becca", "naomi", "rosalia"];
 const emoteList = ["becca", "naomi"];
 const outfitList = ["naomi"];
 const portraitsList = ["becca", "beccalia", "naomi", "rosalia"];
-const posesList = ["becca", "beccalia", "naomi", "novas", "rosalia"];
+const posesList = ["becca", "beccalia", "naomi", "novas", "rosalia", "melody"];
 
 const getFiles = async () => {
   const raw = await fetch(`https://cdn.naomi.lgbt`);
