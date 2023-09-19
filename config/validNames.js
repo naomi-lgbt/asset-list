78f57992444649b1e01c45ed2137a633f9544dc4
Naomi Carrigan <nhcarrigan@gmail.com>
2023-09-19 13:06:46 -0700

    feat: add spell checking

---
+++ config/validNames.js
@@ -0,0 +1,13 @@
+/**
+ * This file contains names that are valid, even though they aren't words.
+ */
+module.exports = [
+  "Becca",
+  "Lyria",
+  "Naomi",
+  "Rosalia",
+  "Nightsong",
+  "Beccalia",
+  "Lillith",
+  "Nashi",
+];
