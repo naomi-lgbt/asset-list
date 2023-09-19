9ed64a5676184436a860381e4ab3c47802618dd6
Naomi Carrigan <nhcarrigan@gmail.com>
2023-09-19 11:26:08 -0700

    chore: clean up structure of other tests

--- test/data.spec.ts
+++ test/data.spec.ts
@@ -15,209 +15,218 @@ const outfitList = ["naomi"];
 const portraitsList = ["becca", "beccalia", "naomi", "rosalia"];
 const posesList = ["becca", "beccalia", "naomi", "novas", "rosalia", "melody"];
 
-suite("Asset data", () => {
-  for (const ns of adventureList) {
-    test(`should have unique names and filenames for ${ns}`, async () => {
-      const json = (await import(`../json/${ns}/adventures.json`))
-        .default as AdventureData[];
-      const fileNames = json.map((a) => a.fileName);
-      for (const file of fileNames) {
-        assert.lengthOf(
-          fileNames.filter((f) => f === file),
-          1,
-          `${file} is duplicated in ${ns}`
-        );
-      }
-      const names = json.map((a) => a.game);
-      for (const name of names) {
-        assert.lengthOf(
-          names.filter((n) => n === name),
-          1,
-          `${name} is duplicated in ${ns}`
-        );
-      }
-    });
+suite("Data Validation:", () => {
+  suite("Adventures", () => {
+    for (const ns of adventureList) {
+      test(`should have unique names and filenames for ${ns}`, async () => {
+        const json = (await import(`../json/${ns}/adventures.json`))
+          .default as AdventureData[];
+        const fileNames = json.map((a) => a.fileName);
+        for (const file of fileNames) {
+          assert.lengthOf(
+            fileNames.filter((f) => f === file),
+            1,
+            `${file} is duplicated in ${ns}`
+          );
+        }
+        const names = json.map((a) => a.game);
+        for (const name of names) {
+          assert.lengthOf(
+            names.filter((n) => n === name),
+            1,
+            `${name} is duplicated in ${ns}`
+          );
+        }
+      });
+      test(`should not have similar descriptions for ${ns}`, async () => {
+        const json = (await import(`../json/${ns}/adventures.json`))
+          .default as AdventureData[];
+        const descriptions = json.map((a) => a.description);
+        for (let i = 0; i < json.length - 1; i++) {
+          const target = descriptions[i];
+          const closest = findBestMatch(
+            target,
+            descriptions.slice(i + 1)
+          ).bestMatch;
+          assert.isBelow(
+            closest.rating,
+            0.9,
+            `${target} is too similar to ${closest.target}`
+          );
+        }
+      });
+    }
+  });
 
-    test(`should not have similar descriptions for ${ns}`, async () => {
-      const json = (await import(`../json/${ns}/adventures.json`))
-        .default as AdventureData[];
-      const descriptions = json.map((a) => a.description);
-      for (let i = 0; i < json.length - 1; i++) {
-        const target = descriptions[i];
-        const closest = findBestMatch(
-          target,
-          descriptions.slice(i + 1)
-        ).bestMatch;
-        assert.isBelow(
-          closest.rating,
-          0.9,
-          `${target} is too similar to ${closest.target}`
-        );
-      }
-    });
-  }
+  suite("Emotes", () => {
+    for (const ns of emoteList) {
+      test(`should have unique names and filenames for ${ns}`, async () => {
+        const json = (await import(`../json/${ns}/emotes.json`))
+          .default as EmoteData[];
+        const fileNames = json.map((a) => a.fileName);
+        for (const file of fileNames) {
+          assert.lengthOf(
+            fileNames.filter((f) => f === file),
+            1,
+            `${file} is duplicated in ${ns}`
+          );
+        }
+        const names = json.map((a) => a.name);
+        for (const name of names) {
+          assert.lengthOf(
+            names.filter((n) => n === name),
+            1,
+            `${name} is duplicated in ${ns}`
+          );
+        }
+      });
 
-  for (const ns of emoteList) {
-    test(`should have unique names and filenames for ${ns}`, async () => {
-      const json = (await import(`../json/${ns}/emotes.json`))
-        .default as EmoteData[];
-      const fileNames = json.map((a) => a.fileName);
-      for (const file of fileNames) {
-        assert.lengthOf(
-          fileNames.filter((f) => f === file),
-          1,
-          `${file} is duplicated in ${ns}`
-        );
-      }
-      const names = json.map((a) => a.name);
-      for (const name of names) {
-        assert.lengthOf(
-          names.filter((n) => n === name),
-          1,
-          `${name} is duplicated in ${ns}`
-        );
-      }
-    });
+      test(`should not have similar descriptions for ${ns}`, async () => {
+        const json = (await import(`../json/${ns}/emotes.json`))
+          .default as EmoteData[];
+        const descriptions = json.map((a) => a.description);
+        for (let i = 0; i < json.length - 1; i++) {
+          const target = descriptions[i];
+          const closest = findBestMatch(
+            target,
+            descriptions.slice(i + 1)
+          ).bestMatch;
+          assert.isBelow(
+            closest.rating,
+            0.9,
+            `${target} is too similar to ${closest.target}`
+          );
+        }
+      });
+    }
+  });
 
-    test(`should not have similar descriptions for ${ns}`, async () => {
-      const json = (await import(`../json/${ns}/emotes.json`))
-        .default as EmoteData[];
-      const descriptions = json.map((a) => a.description);
-      for (let i = 0; i < json.length - 1; i++) {
-        const target = descriptions[i];
-        const closest = findBestMatch(
-          target,
-          descriptions.slice(i + 1)
-        ).bestMatch;
-        assert.isBelow(
-          closest.rating,
-          0.9,
-          `${target} is too similar to ${closest.target}`
-        );
-      }
-    });
-  }
+  suite("Outfits", () => {
+    for (const ns of outfitList) {
+      test(`should have unique names and filenames for ${ns}`, async () => {
+        const json = (await import(`../json/${ns}/outfits.json`))
+          .default as OutfitData[];
+        const fileNames = json.map((a) => a.fileName);
+        for (const file of fileNames) {
+          assert.lengthOf(
+            fileNames.filter((f) => f === file),
+            1,
+            `${file} is duplicated in ${ns}`
+          );
+        }
+        const names = json.map((a) => a.name);
+        for (const name of names) {
+          assert.lengthOf(
+            names.filter((n) => n === name),
+            1,
+            `${name} is duplicated in ${ns}`
+          );
+        }
+      });
 
-  for (const ns of outfitList) {
-    test(`should have unique names and filenames for ${ns}`, async () => {
-      const json = (await import(`../json/${ns}/outfits.json`))
-        .default as OutfitData[];
-      const fileNames = json.map((a) => a.fileName);
-      for (const file of fileNames) {
-        assert.lengthOf(
-          fileNames.filter((f) => f === file),
-          1,
-          `${file} is duplicated in ${ns}`
-        );
-      }
-      const names = json.map((a) => a.name);
-      for (const name of names) {
-        assert.lengthOf(
-          names.filter((n) => n === name),
-          1,
-          `${name} is duplicated in ${ns}`
-        );
-      }
-    });
+      test(`should not have similar descriptions for ${ns}`, async () => {
+        const json = (await import(`../json/${ns}/outfits.json`))
+          .default as OutfitData[];
+        const descriptions = json.map((a) => a.description);
+        for (let i = 0; i < json.length - 1; i++) {
+          const target = descriptions[i];
+          const closest = findBestMatch(
+            target,
+            descriptions.slice(i + 1)
+          ).bestMatch;
+          assert.isBelow(
+            closest.rating,
+            0.9,
+            `${target} is too similar to ${closest.target}`
+          );
+        }
+      });
+    }
+  });
 
-    test(`should not have similar descriptions for ${ns}`, async () => {
-      const json = (await import(`../json/${ns}/outfits.json`))
-        .default as OutfitData[];
-      const descriptions = json.map((a) => a.description);
-      for (let i = 0; i < json.length - 1; i++) {
-        const target = descriptions[i];
-        const closest = findBestMatch(
-          target,
-          descriptions.slice(i + 1)
-        ).bestMatch;
-        assert.isBelow(
-          closest.rating,
-          0.9,
-          `${target} is too similar to ${closest.target}`
-        );
-      }
-    });
-  }
+  suite("Portraits", () => {
+    for (const ns of portraitsList) {
+      test(`should have unique names and filenames for ${ns}`, async () => {
+        const json = (await import(`../json/${ns}/portraits.json`))
+          .default as PortraitsData[];
+        const fileNames = json.map((a) => a.fileName);
+        for (const file of fileNames) {
+          assert.lengthOf(
+            fileNames.filter((f) => f === file),
+            1,
+            `${file} is duplicated in ${ns}`
+          );
+        }
+        const names = json.map((a) => a.name);
+        for (const name of names) {
+          assert.lengthOf(
+            names.filter((n) => n === name),
+            1,
+            `${name} is duplicated in ${ns}`
+          );
+        }
+      });
 
-  for (const ns of portraitsList) {
-    test(`should have unique names and filenames for ${ns}`, async () => {
-      const json = (await import(`../json/${ns}/portraits.json`))
-        .default as PortraitsData[];
-      const fileNames = json.map((a) => a.fileName);
-      for (const file of fileNames) {
-        assert.lengthOf(
-          fileNames.filter((f) => f === file),
-          1,
-          `${file} is duplicated in ${ns}`
-        );
-      }
-      const names = json.map((a) => a.name);
-      for (const name of names) {
-        assert.lengthOf(
-          names.filter((n) => n === name),
-          1,
-          `${name} is duplicated in ${ns}`
-        );
-      }
-    });
+      test(`should not have similar descriptions for ${ns}`, async () => {
+        const json = (await import(`../json/${ns}/portraits.json`))
+          .default as PortraitsData[];
+        const descriptions = json.map((a) => a.description);
+        for (let i = 0; i < json.length - 1; i++) {
+          const target = descriptions[i];
+          const closest = findBestMatch(
+            target,
+            descriptions.slice(i + 1)
+          ).bestMatch;
+          assert.isBelow(
+            closest.rating,
+            0.9,
+            `${target} is too similar to ${closest.target}`
+          );
+        }
+      });
+    }
+  });
 
-    test(`should not have similar descriptions for ${ns}`, async () => {
-      const json = (await import(`../json/${ns}/portraits.json`))
-        .default as PortraitsData[];
-      const descriptions = json.map((a) => a.description);
-      for (let i = 0; i < json.length - 1; i++) {
-        const target = descriptions[i];
-        const closest = findBestMatch(
-          target,
-          descriptions.slice(i + 1)
-        ).bestMatch;
-        assert.isBelow(
-          closest.rating,
-          0.9,
-          `${target} is too similar to ${closest.target}`
-        );
-      }
-    });
-  }
+  suite("Poses", () => {
+    for (const ns of posesList) {
+      test(`should have unique names and filenames for ${ns}`, async () => {
+        const json = (await import(`../json/${ns}/poses.json`))
+          .default as PosesData[];
+        const fileNames = json.map((a) => a.fileName);
+        for (const file of fileNames) {
+          assert.lengthOf(
+            fileNames.filter((f) => f === file),
+            1,
+            `${file} is duplicated in ${ns}`
+          );
+        }
+        const names = json.map((a) => a.name);
+        for (const name of names) {
+          assert.lengthOf(
+            names.filter((n) => n === name),
+            1,
+            `${name} is duplicated in ${ns}`
+          );
+        }
+      });
 
-  for (const ns of posesList) {
-    test(`should have unique names and filenames for ${ns}`, async () => {
-      const json = (await import(`../json/${ns}/poses.json`))
-        .default as PosesData[];
-      const fileNames = json.map((a) => a.fileName);
-      for (const file of fileNames) {
-        assert.lengthOf(
-          fileNames.filter((f) => f === file),
-          1,
-          `${file} is duplicated in ${ns}`
-        );
-      }
-      const names = json.map((a) => a.name);
-      for (const name of names) {
-        assert.lengthOf(
-          names.filter((n) => n === name),
-          1,
-          `${name} is duplicated in ${ns}`
-        );
-      }
-    });
-
-    test(`should not have similar descriptions for ${ns}`, async () => {
-      const json = (await import(`../json/${ns}/poses.json`))
-        .default as PosesData[];
-      const descriptions = json.map((a) => a.description);
-      for (let i = 0; i < json.length - 1; i++) {
-        const target = descriptions[i];
-        const closest = findBestMatch(
-          target,
-          descriptions.slice(i + 1)
-        ).bestMatch;
-        assert.isBelow(
-          closest.rating,
-          0.9,
-          `${target} is too similar to ${closest.target}`
-        );
-      }
-    });
-  }
+      test(`should not have similar descriptions for ${ns}`, async () => {
+        const json = (await import(`../json/${ns}/poses.json`))
+          .default as PosesData[];
+        const descriptions = json.map((a) => a.description);
+        for (let i = 0; i < json.length - 1; i++) {
+          const target = descriptions[i];
+          const closest = findBestMatch(
+            target,
+            descriptions.slice(i + 1)
+          ).bestMatch;
+          assert.isBelow(
+            closest.rating,
+            0.9,
+            `${target} is too similar to ${closest.target}`
+          );
+        }
+      });
+    }
+  });
 });
--- test/schema.spec.ts
+++ test/schema.spec.ts
@@ -6,117 +6,127 @@ const outfitList = ["naomi"];
 const portraitsList = ["becca", "beccalia", "naomi", "rosalia"];
 const posesList = ["becca", "beccalia", "naomi", "novas", "rosalia"];
 
-suite("Adventure schemas", () => {
-  test("should all exist", () => {
-    assert.equal(true, true);
-  });
-  for (const ns of adventureList) {
-    test(`should have the correct properties for ${ns}`, async () => {
-      const keys = ["fileName", "game", "alt", "description"];
-      const json = (await import(`../json/${ns}/adventures.json`)).default;
-      for (const object of json) {
-        assert.containsAllKeys(object, keys);
-        assert.lengthOf(Object.keys(object), keys.length);
-        for (const key of Object.keys(object)) {
-          assert.isString(object[key]);
-          assert.isNotEmpty(object[key]);
-        }
-      }
+suite("Schema Validation:", () => {
+  suite("Adventures", () => {
+    test("should all exist", () => {
+      assert.equal(true, true);
     });
-  }
-});
-
-suite("Emote schemas", () => {
-  test("should all exist", () => {
-    assert.equal(true, true);
-  });
-
-  for (const ns of emoteList) {
-    test(`should have the correct properties for ${ns}`, async () => {
-      const keys = ["fileName", "name", "alt", "description"];
-      const json = (await import(`../json/${ns}/emotes.json`)).default;
-      for (const object of json) {
-        assert.containsAllKeys(object, keys);
-        assert.lengthOf(Object.keys(object), keys.length);
-        for (const key of Object.keys(object)) {
-          assert.isString(object[key]);
-          assert.isNotEmpty(object[key]);
-        }
-      }
-    });
-  }
-});
-
-suite("Outfit schemas", () => {
-  test("should all exist", () => {
-    assert.equal(true, true);
-  });
-  for (const ns of outfitList) {
-    test(`should have the correct properties for ${ns}`, async () => {
-      const keys = ["name", "fileName", "description", "alt", "credits"];
-      const json = (await import(`../json/${ns}/outfits.json`)).default;
-      for (const object of json) {
-        assert.containsAllKeys(object, keys);
-        assert.lengthOf(Object.keys(object), keys.length);
-        for (const key of Object.keys(object)) {
-          if (key === "credits") {
-            // credits can be empty as it falls back to a default message
-            assert.isObject(object[key]);
-          } else {
+    for (const ns of adventureList) {
+      test(`should have the correct properties for ${ns}`, async () => {
+        const keys = ["fileName", "game", "alt", "description"];
+        const json = (await import(`../json/${ns}/adventures.json`)).default;
+        for (const object of json) {
+          assert.containsAllKeys(object, keys);
+          assert.lengthOf(Object.keys(object), keys.length);
+          for (const key of Object.keys(object)) {
             assert.isString(object[key]);
             assert.isNotEmpty(object[key]);
           }
         }
-      }
-    });
-  }
-});
-
-suite("Portraits schemas", () => {
-  test("should all exist", () => {
-    assert.equal(true, true);
-  });
-  for (const ns of portraitsList) {
-    test(`should have the correct properties for ${ns}`, async () => {
-      const keys = ["fileName", "name", "artist", "url", "alt", "description"];
-      const json = (await import(`../json/${ns}/portraits.json`)).default;
-      for (const object of json) {
-        assert.containsAllKeys(object, keys);
-        assert.lengthOf(Object.keys(object), keys.length);
-        for (const key of Object.keys(object)) {
-          assert.isString(object[key]);
-          assert.isNotEmpty(object[key]);
-        }
-      }
-    });
-  }
-});
-
-suite("Picrew schema", () => {
-  test("should be all strings", async () => {
-    const json = (await import(`../json/naomi/picrew.json`)).default;
-    for (const object of json) {
-      assert.isString(object);
-      assert.isNotEmpty(object);
+      });
     }
   });
-});
 
-suite("Poses schemas", () => {
-  test("should all exist", () => {
-    assert.equal(true, true);
-  });
-  for (const ns of posesList) {
-    test(`should have the correct properties for ${ns}`, async () => {
-      const keys = ["fileName", "name", "alt", "description"];
-      const json = (await import(`../json/${ns}/poses.json`)).default;
-      for (const object of json) {
-        assert.containsAllKeys(object, keys);
-        assert.lengthOf(Object.keys(object), keys.length);
-        for (const key of Object.keys(object)) {
-          assert.isString(object[key]);
+  suite("Emotes", () => {
+    test("should all exist", () => {
+      assert.equal(true, true);
+    });
+
+    for (const ns of emoteList) {
+      test(`should have the correct properties for ${ns}`, async () => {
+        const keys = ["fileName", "name", "alt", "description"];
+        const json = (await import(`../json/${ns}/emotes.json`)).default;
+        for (const object of json) {
+          assert.containsAllKeys(object, keys);
+          assert.lengthOf(Object.keys(object), keys.length);
+          for (const key of Object.keys(object)) {
+            assert.isString(object[key]);
+            assert.isNotEmpty(object[key]);
+          }
         }
+      });
+    }
+  });
+
+  suite("Outfits", () => {
+    test("should all exist", () => {
+      assert.equal(true, true);
+    });
+    for (const ns of outfitList) {
+      test(`should have the correct properties for ${ns}`, async () => {
+        const keys = ["name", "fileName", "description", "alt", "credits"];
+        const json = (await import(`../json/${ns}/outfits.json`)).default;
+        for (const object of json) {
+          assert.containsAllKeys(object, keys);
+          assert.lengthOf(Object.keys(object), keys.length);
+          for (const key of Object.keys(object)) {
+            if (key === "credits") {
+              // credits can be empty as it falls back to a default message
+              assert.isObject(object[key]);
+            } else {
+              assert.isString(object[key]);
+              assert.isNotEmpty(object[key]);
+            }
+          }
+        }
+      });
+    }
+  });
+
+  suite("Portraits", () => {
+    test("should all exist", () => {
+      assert.equal(true, true);
+    });
+    for (const ns of portraitsList) {
+      test(`should have the correct properties for ${ns}`, async () => {
+        const keys = [
+          "fileName",
+          "name",
+          "artist",
+          "url",
+          "alt",
+          "description",
+        ];
+        const json = (await import(`../json/${ns}/portraits.json`)).default;
+        for (const object of json) {
+          assert.containsAllKeys(object, keys);
+          assert.lengthOf(Object.keys(object), keys.length);
+          for (const key of Object.keys(object)) {
+            assert.isString(object[key]);
+            assert.isNotEmpty(object[key]);
+          }
+        }
+      });
+    }
+  });
+
+  suite("Picrew", () => {
+    test("should be all strings", async () => {
+      const json = (await import(`../json/naomi/picrew.json`)).default;
+      for (const object of json) {
+        assert.isString(object);
+        assert.isNotEmpty(object);
       }
     });
-  }
+  });
+
+  suite("Poses", () => {
+    test("should all exist", () => {
+      assert.equal(true, true);
+    });
+    for (const ns of posesList) {
+      test(`should have the correct properties for ${ns}`, async () => {
+        const keys = ["fileName", "name", "alt", "description"];
+        const json = (await import(`../json/${ns}/poses.json`)).default;
+        for (const object of json) {
+          assert.containsAllKeys(object, keys);
+          assert.lengthOf(Object.keys(object), keys.length);
+          for (const key of Object.keys(object)) {
+            assert.isString(object[key]);
+            assert.isNotEmpty(object[key]);
+          }
+        }
+      });
+    }
+  });
 });
